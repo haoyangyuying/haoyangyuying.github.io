e151974a58272c14becdc8954b34af92d69dcc8a
Howard Yang <howard@shoplineapp.com>
2020-04-10 14:04:53 +0800

    update js file

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,27 +1,27 @@
-if (/iP(?:od|hone)/.test(navigator.userAgent)) {
-    // deletable at least, i.e.
-    //  if (window.gestureStart && dontWantIt) {
-    //    document.removeEventListener("gesturestart", gestureStart);
-    //    delete window.gestureStart;
-    //  }
-    this.gestureStart = (function(metas, i){
-      function gestureStart() {
-        // address .length once per loop
-        for (i = metas.length; i--;) {
-          if (metas[i].name == "viewport") {
-            metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-          }
-        }
-      }
-      // address .length once per loop
-      for (i = metas.length; i--;) {
-        if (metas[i].name == "viewport") {
-          metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-        }
-      }
-      // I might argue it should be on capture phase here
-      document.addEventListener("gesturestart", gestureStart);
-      // to be able to address it out there
-      return gestureStart;
-    }(document.getElementsByTagName('meta')));
-  }
+(function(document) {
+    var metas = document.getElementsByTagName('meta'),
+        changeViewportContent = function(content) {
+            for (var i = 0; i < metas.length; i++) {
+                if (metas[i].name == "viewport") {
+                    metas[i].content = content;
+                }
+            }
+        },
+        initialize = function() {
+            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+        },
+        gestureStart = function() {
+            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+        },
+        gestureEnd = function() {
+            initialize();
+        };
+
+
+    if (navigator.userAgent.match(/iPhone/i)) {
+        initialize();
+
+        document.addEventListener("touchstart", gestureStart, false);
+        document.addEventListener("touchend", gestureEnd, false);
+    }
+})(document);
